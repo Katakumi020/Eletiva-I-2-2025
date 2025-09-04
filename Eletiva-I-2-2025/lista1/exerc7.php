1ca53a0c650869aa1e15a5e1d5ddee53d324015d
Alec B <ale13black@gmail.com>
2025-09-04 20:26:35 -0300

    mais um

--- Eletiva-I-2-2025/lista1/exerc7.php
+++ Eletiva-I-2-2025/lista1/exerc7.php
@@ -3,15 +3,15 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 6 da lista 1</title>
+<title>Exercício 7 da lista 1</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1>Exercício 6 da lista 1</h1>
+<h1>Exercício 7 da lista 1</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="temp" class="form-label">digite a temperatura em Graus Celsius:</label>
+              <label for="temp" class="form-label">digite a temperatura em Graus fahrenheit:</label>
               <input type="number" id="temp" name="temp" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
@@ -19,11 +19,11 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $temperatura = $_POST['temp'];
-    $conversao = ($temperatura * (9/5)) + 32; // peguei a formula da internet, acho que ta certaaaaaa
-    echo "<p> A temperatura de $temperatura ºC em fahrenheit é: $conversao ºF </p>";
+    $conversao = ($temperatura - 32) * 5/9; // inverti a outra formula
+    $numero_formatado = number_format($conversao, 2, ','); 
+    echo "<p> A temperatura de $temperatura ºF em Celsius é: $numero_formatado ºC </p>";
     }
     ?>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
