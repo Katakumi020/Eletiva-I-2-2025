2ffeb6e65eb28ffc4634a0bc9c4c27ae7ca4c440
Alec B <ale13black@gmail.com>
2025-09-04 22:05:13 -0300

    lalallalala

--- Eletiva-I-2-2025/lista1/exerc18.php
+++ Eletiva-I-2-2025/lista1/exerc18.php
@@ -3,12 +3,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 17 da lista 1</title>
+<title>Exercício 18 da lista 1</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1>Exercício 17 da lista 1</h1>
+<h1>Exercício 18 da lista 1</h1>
 <form method="post">
 <div class="mb-3">
               <label for="valor" class="form-label">digite o valor do produto:</label>
@@ -29,9 +29,9 @@
     $valor = $_POST['valor'];
     $juros = $_POST['juros'];
     $periodo = $_POST['periodo'];
-    $jurossimples = $valor * ($juros / 100) * $periodo;
+    $jurossimples = $valor * (1+ ($juros / 100)) ** $periodo;
     $numero_formatado = number_format($jurossimples, 2, ',', '.');
-    echo "<p> O valor gerado pelo juros simples após o período determinado é: R$$numero_formatado </p>";
+    echo "<p> O valor gerado pelo juros composto após o período determinado é: R$$numero_formatado </p>";
     }
     ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
