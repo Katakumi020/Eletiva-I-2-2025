579a294da40e67d4eb7f694b58518615cfa0ebaa
Alec B <ale13black@gmail.com>
2025-09-04 21:06:36 -0300

    nao sei mais oque escrever

---
+++ Eletiva-I-2-2025/lista1/exerc9.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 7 da lista 1</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Exercício 7 da lista 1</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="raio" class="form-label">digite a raio do círculo em metros:</label>
+              <input type="number" id="raio" step="0.01" name="raio" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    define ("pi", 3.1415926536);
+    $raio = $_POST['raio'];
+    $area = pi * pow($raio, 2);
+    $numero_formatado = number_format($area, 2, ',');
+    echo "<p> A área do circulo é: $numero_formatado m²</p>";
+    }
+    ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
