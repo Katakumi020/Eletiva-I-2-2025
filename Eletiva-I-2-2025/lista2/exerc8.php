b8ebf031cd2bf02d5eb934770710470a40125da7
Katakumi020 <ale13black@gmail.com>
2025-09-23 21:33:20 -0300

    2344453

---
+++ Eletiva-I-2-2025/lista2/exerc8.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Exercício 8 lista 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-4">
+<h1>Exercício 7 lista 2</h1>
+<form method="post">
+  <div class="mb-3">
+    <label for="num1" class="form-label">Digite um número:</label>
+    <input type="number" id="num1" name="num1" class="form-control" required>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $numero = (int)$_POST['num1'];
+    echo "<div class='alert alert-warning mt-3'>Contagem regressiva: ";
+    do {
+        echo $numero . " ";
+        $numero--;
+    } while ($numero >= 1);
+    echo "</div>";
+}
+?>
+</div>
+</body>
+</html>
