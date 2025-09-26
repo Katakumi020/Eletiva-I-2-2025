0b189a574b454e2795761cf1423c0deb22cf4bd7
Katakumi020 <ale13black@gmail.com>
2025-09-25 21:30:23 -0300

    voce ja assistiu dexter?

--- Eletiva-I-2-2025/lista3/exerc4.php
+++ Eletiva-I-2-2025/lista3/exerc4.php
@@ -3,12 +3,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 3 da lista 3</title>
+<title>Exercício 4 da lista 3</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1>Exercício 3 da lista 3</h1>
+<h1>Exercício 4 da lista 3</h1>
 <form method="post">
     <div class="mb-3">
     <label for="dia" class="form-label">Digite o dia:</label>
---
+++ Eletiva-I-2-2025/lista3/exerc5.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 5 da lista 3</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Exercício 5 da lista 3</h1>
+<form method="post">
+  <div class="mb-3">
+    <label for="numero" class="form-label">Digite um número:</label>
+    <input type="number" id="numero" name="numero" class="form-control" step="0.01" required>
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $NUM = $_POST['numero'];
+    $raiz = sqrt($NUM);
+    echo "<p>A raiz quadrada de $NUM é ".number_format($raiz,2,",")."</p>";
+}
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
