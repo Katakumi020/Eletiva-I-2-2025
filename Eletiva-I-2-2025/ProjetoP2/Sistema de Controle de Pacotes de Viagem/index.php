1031b3804d44330652547cc6b79bad4ad50376f4
Katakumi020 <ale13black@gmail.com>
2025-12-02 21:53:28 -0300

    calma

---
+++ Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/index.php
@@ -0,0 +1,4 @@
+<?php require_once 'header.php'; ?>
+<h1>Bem-vindo ao Sistema de Controle de Pacotes</h1>
+<p>Use o menu para gerenciar destinos, clientes, pacotes e vendas.</p>
+<?php require_once 'footer.php'; ?>
