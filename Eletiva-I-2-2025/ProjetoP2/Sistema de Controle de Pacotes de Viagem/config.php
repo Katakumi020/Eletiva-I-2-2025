d7a6eb334e05656ef40725f18b5f61ce6e99ba62
Katakumi020 <ale13black@gmail.com>
2025-12-03 22:56:52 -0300

    alterei uns itens

--- Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/config.php
+++ Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/config.php
@@ -9,4 +9,8 @@ date_default_timezone_set('America/Sao_Paulo');
 
 
 function h($s){ return htmlspecialchars($s, ENT_QUOTES); }
+
+
+$BASE_URL = "http://localhost/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/";
+
 ?>
