f58b4702ae86165566521beef789ba0b34fdfc4e
Katakumi020 <ale13black@gmail.com>
2025-12-03 22:45:43 -0300

    final:?

---
+++ Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/login/login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once 'conexao.php';
+if($_SERVER['REQUEST_METHOD']==='POST'){
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+$conn=conectar();
+$stmt=$conn->prepare('SELECT * FROM usuarios WHERE email=?');
+$stmt->bind_param('s',$email);
+$stmt->execute();
+$user=$stmt->get_result()->fetch_assoc();
+if($user && password_verify($senha,$user['senha'])){
+$_SESSION['user']=$user['nome'];
+$_SESSION['tipo']=$user['tipo'];
+header('Location:index.php'); exit;
+} else $erro="Login incorreto";
+}
+?>
+<form method='post'>
+<label>Email</label><input type='email' name='email' required>
+<label>Senha</label><input type='password' name='senha' required>
+<button>Entrar</button>
+</form>
+<?php if(isset($erro)) echo "<p style='color:red'>$erro</p>"; ?>
