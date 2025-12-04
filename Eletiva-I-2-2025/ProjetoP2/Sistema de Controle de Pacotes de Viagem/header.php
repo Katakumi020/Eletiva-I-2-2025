09313647d8cc383f9a0b5d62e072930e1ddfc0ca
Katakumi020 <ale13black@gmail.com>
2025-12-03 23:28:06 -0300

    cabou

--- Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/header.php
+++ Eletiva-I-2-2025/ProjetoP2/Sistema de Controle de Pacotes de Viagem/header.php
@@ -7,15 +7,16 @@
 <link rel="stylesheet" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/css/arquivo.css">
 </head>
 <body class="p-4">
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 p-3 rounded">
-<a class="navbar-brand" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/index.php">Agência</a>
-<div class="collapse navbar-collapse">
-<ul class="navbar-nav me-auto">
-<li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/destinos/listar_destinos.php">Destinos</a></li>
-<li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/clientes/listar_clientes.php">Clientes</a></li>
-<li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/pacotes/listar_pacotes.php">Pacotes</a></li>
-<li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/vendas/listar_vendas.php">Vendas</a></li>
-</ul>
-</div>
+<nav class="navbar navbar-expand-lg navbar-dark p-3 mb-4 rounded">
+    <a class="navbar-brand" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/index.php">Agência</a>
+
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/destinos/listar_destinos.php">Destinos</a></li>
+            <li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/clientes/listar_clientes.php">Clientes</a></li>
+            <li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/pacotes/listar_pacotes.php">Pacotes</a></li>
+            <li class="nav-item"><a class="nav-link" href="/Sistema%20de%20Controle%20de%20Pacotes%20de%20Viagem/vendas/listar_vendas.php">Vendas</a></li>
+        </ul>
+    </div>
 </nav>
 <div class="container">
